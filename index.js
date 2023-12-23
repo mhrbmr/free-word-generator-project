d80dff9c57a2709476213fcf642111a38d586bfd
mhrbmr <supersuniiside@gmail.com>
2023-12-23 11:15:34 -0700

    Update index.js
    Updated error message text and color

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ async function generateMyWord() {
   const randomWord = await fetchRandomWord.json();
 
   document.getElementById("word").innerHTML = randomWord;
-  console.log(randomWord);
 
   const fetchWordInfo = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`);
   const wordInfo = await fetchWordInfo.json();
 
-  console.log(wordInfo);
-
   if (wordInfo.title === "No Definitions Found") {
-    document.getElementById("definition").innerHTML = "Sorry no dicionary definition found for this word via dictionaryapi.dev";
-    document.getElementById("definition").style.color = "firebrick";
+    document.getElementById("definition").innerHTML = "Sorry, no definition found for this word via dictionaryapi.dev";
+    document.getElementById("definition").style.color = "red";
   } else {
     const meaning = await wordInfo[0].meanings[0].definitions[0].definition;
     document.getElementById("definition").style.color = "black";
