c3b5e86be3fd237964d2e117b132b8e238e84785
mhrbmr <supersuniiside@gmail.com>
2023-12-22 19:37:35 -0700

    Update index.js
    Fixed bug where button click does nothing, occasionally.

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ async function generateMyWord() {
   const fetchRandomWord = await fetch('https://random-word-api.vercel.app/api?words=1');
   const randomWord = await fetchRandomWord.json();
 
+  document.getElementById("word").innerHTML = randomWord;
+  console.log(randomWord);
+
   const fetchWordInfo = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`);
   const wordInfo = await fetchWordInfo.json();
-  
-  const meaning = await wordInfo[0].meanings[0].definitions[0].definition;
 
-  document.getElementById("word").innerHTML = randomWord;
-  document.getElementById("definition").innerHTML = meaning;
+  console.log(wordInfo);
+
+  if (wordInfo.title === "No Definitions Found") {
+    document.getElementById("definition").innerHTML = "Sorry no dicionary definition found for this word via dictionaryapi.dev";
+    document.getElementById("definition").style.color = "firebrick";
+  } else {
+    const meaning = await wordInfo[0].meanings[0].definitions[0].definition;
+    document.getElementById("definition").style.color = "black";
+    document.getElementById("definition").innerHTML = meaning;
+  }
+  
   document.getElementById('word-wrapper').style.display = "block";
 }
