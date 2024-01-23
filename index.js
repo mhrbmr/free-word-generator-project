584ddb8c7559cdb83a0e4c8469101ff13d94afaa
mhrbmr <supersuniiside@gmail.com>
2024-01-22 17:17:08 -0700

    Update index.js
    Updated code to change the behavior of the display of word and description after load from loading in a staggered fashion to loading sooner together(if not at the same time)

--- index.js
+++ index.js
@@ -17,18 +17,20 @@ async function generateMyWord() {
   const fetchRandomWord = await fetch('https://random-word-api.vercel.app/api?words=1');
   const randomWord = await fetchRandomWord.json();
 
-  document.getElementById("word").innerHTML = randomWord;
+  # document.getElementById("word").innerHTML = randomWord;
 
   const fetchWordInfo = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`);
   const wordInfo = await fetchWordInfo.json();
 
   if (wordInfo.title === "No Definitions Found") {
+    document.getElementById("word").innerHTML = randomWord;
     document.getElementById("definition").innerHTML = "Sorry, no definition found for this word via dictionaryapi.dev";
     document.getElementById("definition").style.color = "firebrick";
     document.getElementById("sourc-url").href = 'https://en.wiktionary.org/wiki/';
     document.getElementById("sourc-url").innerHTML = 'https://en.wiktionary.org/wiki/';
   } else {
     const meaning = await wordInfo[0].meanings[0].definitions[0].definition;
+    document.getElementById("word").innerHTML = randomWord;
     document.getElementById("definition").style.color = "black";
     document.getElementById("definition").innerHTML = meaning;
     document.getElementById("sourc-url").href = `https://en.wiktionary.org/wiki/${randomWord}`;
